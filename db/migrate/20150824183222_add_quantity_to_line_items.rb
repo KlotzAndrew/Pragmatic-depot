3ad5a91239761db6c485597c15b208bdce95eb8b
Andrew Klotz <andrew.klotz@hotmail.com>
2015-08-29 13:18:02 -0400

    pg debugg migration

--- db/migrate/20150824183222_add_quantity_to_line_items.rb
+++ db/migrate/20150824183222_add_quantity_to_line_items.rb
@@ -1,5 +1,5 @@
 class AddQuantityToLineItems < ActiveRecord::Migration
   def change
-    add_column :line_items, :quantity, :integer​, default: 1
+    add_column :line_items, :quntity, :integer, default: 0
   end
 end
